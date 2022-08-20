6566409323fab1ff77f343641e750e83eab10943
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-08-20 17:22:02 +0900

    pass test

--- cmd/csv_test.go
+++ cmd/csv_test.go
@@ -7,11 +7,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestToCsvString(t *testing.T) {
+func TestToCsvString1File(t *testing.T) {
 	dir, _ := os.Getwd()
 	filepath := dir + "/../test_resources/test1.parquet"
-	actual := toCsvString(filepath, TableConfigDefault)
+	actual := toCsvString([]string{filepath}, TableConfigDefault)
 	expected := `one,two,three
+-1,foo,true
+<nil>,bar,false
+2.5,baz,true`
+	assert.Equal(t, expected, actual, "they should be equal")
+}
+func TestToCsvString2File(t *testing.T) {
+	dir, _ := os.Getwd()
+	filepath1 := dir + "/../test_resources/test1.parquet"
+	filepath2 := dir + "/../test_resources/test2.parquet"
+	actual := toCsvString([]string{filepath1, filepath2}, TableConfigDefault)
+	expected := `one,two,three
+-1,foo,true
+<nil>,bar,false
+2.5,baz,true
 -1,foo,true
 <nil>,bar,false
 2.5,baz,true`
